71d4d80bb77bad1f7e2cba972e96cd738340bb96
m1212e <14091540+m1212e@users.noreply.github.com>
2025-11-27 19:28:30 +0100

    wip

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { client } from "$lib/api/rumbleClient/client";
+
+  const res = await client.query.me({
+    email: true,
+    family_name: true,
+    given_name: true,
+    locale: true,
+    phone: true,
+    preferred_username: true,
+    sub: true,
+  });
+  console.log(res);
+</script>
+
+{JSON.stringify(res, null, 2)}
